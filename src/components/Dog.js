13d7b98539f16a5a2a3723b2f3cedf293aedc22e
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-01-03 11:38:13 -0800

    Added additional basic code to Dog and DogBuddy componenets

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Dog.css";
 
-const Dog = () => {
-  return <h2>Dog</h2>;
-};
+class Dog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name,
+      id: this.props.id,
+      age: this.props.age,
+      size: this.props.size,
+      vaccinated: this.props.vaccinated,
+      about: this.props.about,
+      breed: this.props.breed,
+      preferredPlayBuddy: this.props.preferredPlayBuddy
+    };
+  }
+}
 
 Dog.propTypes = {
   name: PropTypes.string,
--- src/components/DogBuddy.js
+++ src/components/DogBuddy.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./DogBuddy.css";
 
-const DogBuddy = props => {
-  return <h2>Dog Buddy</h2>;
-};
+class DogBuddy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      dogs: [],
+      messages: [],
+      alertMessage: ""
+    };
+  }
+
+  changeMessage = message => {
+    this.setState({ alertMessage: message });
+    setTimeout(() => this.setState({ alertMessage: "" }), 2500);
+  };
+
+  //load dogs axios get method saved as loadDogs
+
+  // load users axios get method saved as loadUsers
+
+  // load messages axios get method saved as loadMessages
+
+  // componentDidMount method that loads the users/dogs/messages
+
+  render() {
+    return <h1>Dog Buddy App</h1>;
+    // Add ul for router links to home, search, users, dogs, dashboard, maybe sign out/sign up?
+    // Add a router with routes  to users/dogs /passing the route this.state.users or dogs
+  }
+}
 
 DogBuddy.propTypes = {};
 
