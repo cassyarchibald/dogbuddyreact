f7c1901a0cf0e0c1b707d9eec0f3bd9a94d4aeca
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-01-05 16:26:08 -0800

    Added person's dogs to their state in person componeent/view

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,33 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./Person.css";
+import axios from "axios";
+import Dog from "./Dog";
+import DogCollection from "./DogCollection";
 
-const Person = props => {
-  const {
-    firstName,
-    lastName,
-    city,
-    state,
-    zipCode,
-    about,
-    photo,
-    addUserCallback,
-    dogLink
-  } = props;
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: props.firstName,
+      lastName: props.lastName,
+      city: props.city,
+      state: props.state,
+      zipCode: props.zipCode,
+      about: props.about,
+      photo: props.photo,
+      addUserCallback: props.addUserCallback,
+      dogLink: props.dogLink,
+      dogs: [],
+      errorMessages: []
+    };
+  }
 
-  // Do axios request to render person's dogs
-  return (
-    <div className="card d-inline-block">
-      <h3 className="text-center">
-        {firstName} {lastName}
-      </h3>
-      <p>City: {city}</p>
-      <p>State: {state}</p>
-      <p>Zip Code: {zipCode}</p>
-      <p>About: {about}</p>
-    </div>
-  );
-};
+  loadDogs() {
+    axios
+      .get(this.state.dogLink)
+      .then(response => {
+        const dogComponents = response.data._embedded.dogs.map(dog => {
+          //console.log(dog);
+          return (
+            <Dog
+              key={dog.resourceId}
+              id={dog.resourceId}
+              name={dog.name}
+              age={dog.age}
+              size={dog.size}
+              vaccinated={dog.vaccinated}
+              about={dog.about}
+              photo={dog.photo}
+              breed={dog.breed}
+              personLink={dog._links.person.href}
+              preferredPlayBuddy={dog.preferredPlayBuddy}
+            />
+          );
+        });
+
+        this.setState({
+          dogs: dogComponents
+        });
+      })
+      .catch(error => {
+        this.changeMessage(error.message);
+      });
+  }
+
+  componentDidMount() {
+    this.loadDogs();
+  }
+
+  render() {
+    return (
+      <div className="card d-inline-block">
+        <h3 className="text-center">
+          {this.state.firstName} {this.state.lastName}
+        </h3>
+        <p>City: {this.state.city}</p>
+        <p>State: {this.state.state}</p>
+        <p>Zip Code: {this.state.zipCode}</p>
+        <p>About: {this.state.about}</p>
+        <div className="dog-collection">
+          <DogCollection
+            dogComponentsCollection={this.state.dogs}
+            addDogCallback={this.addDogCallback}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 Person.propTypes = {
   firstName: PropTypes.string,
