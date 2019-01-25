d71fa9b47b7425b8d8fc844ea99898085e2e4bc7
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2019-01-24 19:22:34 -0800

    Updates

---
+++ precache-manifest.8ee3fa72c8369ba430cd02830aca1d52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd84152d48da049bc3d0",
+    "url": "/dogbuddyreact/static/css/main.888729ae.chunk.css"
+  },
+  {
+    "revision": "cd84152d48da049bc3d0",
+    "url": "/dogbuddyreact/static/js/main.cd84152d.chunk.js"
+  },
+  {
+    "revision": "06849a2378cfb496ba40",
+    "url": "/dogbuddyreact/static/css/1.e009322a.chunk.css"
+  },
+  {
+    "revision": "06849a2378cfb496ba40",
+    "url": "/dogbuddyreact/static/js/1.06849a23.chunk.js"
+  },
+  {
+    "revision": "9e61e5fa2ed24d433e6c",
+    "url": "/dogbuddyreact/static/js/runtime~main.9e61e5fa.js"
+  },
+  {
+    "revision": "7692576f4fbb91108348946b63eb2940",
+    "url": "/dogbuddyreact/static/media/signup.7692576f.jpg"
+  },
+  {
+    "revision": "bb75b6aac2dca5824cc0a25e24496bf3",
+    "url": "/dogbuddyreact/static/media/dogs-running.bb75b6aa.jpg"
+  },
+  {
+    "revision": "681ad1e5771b633fcd1aa2758e19a12f",
+    "url": "/dogbuddyreact/static/media/dogsplaying.681ad1e5.jpg"
+  },
+  {
+    "revision": "a810c7e08225af98c48b9a2a086b5a0d",
+    "url": "/dogbuddyreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dogbuddy/precache-manifest.99683161d8ad84162e44329608fda370.js"
+  "/dogbuddyreact/precache-manifest.8ee3fa72c8369ba430cd02830aca1d52.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dogbuddy/index.html", {
+workbox.routing.registerNavigationRoute("/dogbuddyreact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
